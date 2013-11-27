c317a9499e9f691ef999f874f8b5b06b4d932198
Tak Tran <contact@tutaktran.com>
2013-11-27 10:49:24 +0000

    Add sonar data to xively.

--- web/bin/xively.js
+++ web/bin/xively.js
@@ -4,12 +4,14 @@ var API_BASE = "https://api.xively.com/";
 var API_KEY = "m4FrAG9u09PaF45HV7gurw0BTpxvFGIRtaqHVe0IcSVsxAcf";
 var FEED_ID = "1067059460";
 
+var SONAR_PIN = 7;
+
 var needle = require('needle');
+var five = require("johnny-five");
 
 
-var datastreamId = "test";
+var datastreamId = "sonar";
 var xivelyAddDataUrl = API_BASE + "/v2/feeds/" + FEED_ID + ".json";
-var xivelyGetDataUrl = API_BASE + "/v2/feeds/" + FEED_ID + "/datastreams/" + datastreamId;
 
 var headers = {
   "X-ApiKey": API_KEY
@@ -20,20 +22,38 @@ var options = {
   "json": true
 };
 
-var data = {
-  "version":"1.0.0",
-  "datastreams" : [
-    {
-      "id" : datastreamId,
-      "current_value" : "55"
+// Johnny five
+var board = new five.Board();
+
+// How far in cm it is for when there is no detection
+var NO_DETECTION_THRESHOLD = 7.5;
+
+board.on("ready", function() {
+
+  // Create a new `ping` hardware instance.
+  var ping = new five.Ping(SONAR_PIN);
+
+  ping.on("change", function(err, value) {
+    var detectionValue = 0;
+
+    if (this.cm < NO_DETECTION_THRESHOLD) {
+      detectionValue = 1;
     }
-  ]
-};
 
-needle.put(xivelyAddDataUrl, data, options, function(err, resp, body){
-  console.log("put:", resp.output);
+    var data = {
+      "version":"1.0.0",
+      "datastreams" : [
+        {
+          "id" : datastreamId,
+          "current_value": detectionValue
+        }
+      ]
+    };
 
-  needle.get(xivelyGetDataUrl, options, function(err, resp, body){
-    console.log(body);
+    console.log(value, this.cm + "cm away (" + detectionValue + ")");
+
+    needle.put(xivelyAddDataUrl, data, options, function(err, resp, body) {
+      console.log("put:", data, resp.output);
+    });
   });
 });
